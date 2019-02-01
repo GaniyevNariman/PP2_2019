f645ba771a547e07c7faf12f3877a40ba6650cc6
Wrendl <narimanganiyev25@gmail.com>
2019-02-01 14:55:04 +0600

    Update 1.cs

--- Lab1/1.cs
+++ Lab1/1.cs
@@ -10,32 +10,32 @@ namespace ConsoleApp4
 	{
 		static void Main(string[] args)
 		{
-			int x = int.Parse(Console.ReadLine());
-			string s = Console.ReadLine();
-			string[] a = s.Split();
-			int cnt1 = 0;
-			List<int> c = new List<int>();
+			int x = int.Parse(Console.ReadLine()); // первый элемент считал и перевел в int
+			string s = Console.ReadLine(); 
+			string[] a = s.Split(); // поделил строки через пробел и залил в массив
+			int cnt1 = 0; // нужно что бы посчитать сколько у нас prime
+			List<int> c = new List<int>(); // создал List для prime числов
 			for(int i=0; i<x; i++)
 			{
-				int b = int.Parse(a[i]);
-				int cnt = 0;
+				int b = int.Parse(a[i]); // каждый элемент в массиве перевел в int
+				int cnt = 0; // нужно что бы посчитать сколько делителей у числа
 				for(int j=1; j<=b; j++)
 				{
-					if (b % j == 0)
+					if (b % j == 0) //если есть делитель то ++
 					{
 						cnt++;
 					}
 				}
-				if (cnt == 2)
+				if (cnt == 2) // делитель равен 2 означает что это число prime
 				{
 					cnt1++;
 					c.Add(b);
 				}
 			}
-			Console.WriteLine(cnt1);
+			Console.WriteLine(cnt1); // нам нужно вывести сколько у нас prime
 			for(int i=0; i<c.Count; i++)
 			{
-				Console.Write(c[i] + " ");
+				Console.Write(c[i] + " "); // и каждый prime
 			}
 		}
 	}
