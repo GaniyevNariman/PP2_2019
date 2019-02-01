34c4e5ae3c932e2ae75b67409be51f99ac31a3f3
Wrendl <narimanganiyev25@gmail.com>
2019-02-01 14:55:17 +0600

    Update 4.cs

--- Lab1/4.cs
+++ Lab1/4.cs
@@ -8,16 +8,16 @@ namespace ConsoleApp2
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main(string[] args)	
 		{
-			int x = int.Parse(Console.ReadLine());
-			for(int i=0; i<x; i++)
+			int x = int.Parse(Console.ReadLine()); // первый элемент считал и перевел в int
+			for (int i=0; i<x; i++) // создал двойной цикл
 			{
-				for(int j=0; j<=i; j++)
+				for(int j=0; j<=i; j++) // здесь пробигаемся только до i для того что бы в строке было только i звездочек
 				{
-					Console.Write("[*]");
+					Console.Write("[*]"); // выводит звездочки без endl
 				}
-				Console.WriteLine();
+				Console.WriteLine(); // endl для того что бы после каждой строки звездочки опускались вниз
 			}
 		}
 	}
