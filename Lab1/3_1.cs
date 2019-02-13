e6735fcb0c031e64d3a7d0f3682731b4ea30dd8a
Wrendl <narimanganiyev25@gmail.com>
2019-02-13 15:06:53 +0600

    Create 3_1.cs

---
+++ Lab1/3_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] a = (Console.ReadLine()).Split(); // переводим в int первую строку
+			string[] b = new string[n * 2]; // создаем новый массив в которым будут храниться числа с дубликатами
+			for(int i=0; i<n; i++) // пробегаемся по циклу
+			{
+				b[i*2] = a[i]; // и делаем дубликаты
+				b[i*2 + 1] = a[i];
+			}
+
+			for(int i=0; i<n*2; i++) // выводим числа
+			{
+				Console.Write(b[i] + " ");
+			}
+		}
+	}
+}
